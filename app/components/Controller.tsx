18936d98335e89b7cbd1bb0d32085b4c3cb9c5be
pedrofalco <fpfalco@gmail.com>
2026-01-14 16:50:03 -0300

    refactor: optimize Controller component with CSS variables and rounded values

--- app/components/Controller.tsx
+++ app/components/Controller.tsx
@@ -21,17 +21,28 @@ export default function Controller({ selectedMenu = 'Leaderboard', onMenuChange 
 
   return (
     <div 
-      className="pl-2.5 pr-5 py-2.5 bg-black-turtle rounded-full shadow-black-turtle outline outline-black-highlight/10 outline-offset-[-0.60px] inline-flex justify-center items-center gap-5"
+      className="pl-2.5 pr-5 py-2.5 bg-black-turtle rounded-full outline outline-black-highlight/10 -outline-offset-1 inline-flex justify-center items-center gap-5"
+      style={{
+        boxShadow: 'var(--shadow-black-turtle)'
+      }}
     >
       {/* Left side - "What's Included" indicator */}
       <div 
-        className="bg-black-highlight/0 rounded-full shadow-[0px_4px_21.95px_0px_rgba(0,0,0,0.20)] shadow-[inset_0px_0px_10px_0px_rgba(115,243,108,0.16)] outline outline-white/40 outline-offset-[-0.55px] flex justify-start items-center gap-2.5"
+        className="bg-black-highlight/0 rounded-full outline outline-white/40 -outline-offset-1 flex justify-start items-center gap-2.5"
+        style={{
+          boxShadow: 'var(--shadow-black-turtle), inset 0px 0px 10px 0px rgba(115,243,108,0.16)'
+        }}
       >
         {/* Circular indicator with gradient */}
         <div 
-          className="w-20 h-20 relative bg-gradient-to-r from-black-turtle/60 to-green-turtle/0 rounded-full shadow-[inset_0px_0px_6px_0px_rgba(115,243,108,1.00)] outline outline-white/40 outline-offset-[-1.39px] overflow-hidden"
+          className="w-20 h-20 relative bg-linear-to-r from-black-turtle/60 to-green-turtle/0 rounded-full outline outline-white/40 -outline-offset-1 overflow-hidden"
+          style={{
+            boxShadow: 'inset 0px 0px 6px 0px var(--green-turtle)'
+          }}
         >
-          <div className="w-16 h-20 left-0 top-[17px] absolute bg-gradient-to-b from-green-turtle to-green-turtle/0"></div>
+          <div 
+            className="w-16 h-20 left-0 top-[17px] absolute bg-linear-to-b from-green-turtle to-green-turtle/0"
+          ></div>
         </div>
         
         {/* Text label */}
@@ -44,19 +55,22 @@ export default function Controller({ selectedMenu = 'Leaderboard', onMenuChange 
       </div>
 
       {/* Right side - Menu options */}
-      <div className="w-[454.14px] rounded-md shadow-[0px_4px_6px_0px_rgba(0,0,0,0.09)] flex justify-end items-start gap-10 overflow-hidden">
-        <div className="p-[5px] flex justify-start items-start gap-4">
+      <div className="w-[454px] rounded-md shadow-[0px_4px_6px_0px_rgba(0,0,0,0.09)] flex justify-end items-start gap-10 overflow-hidden">
+        <div className="p-1 flex justify-start items-start gap-4">
           {menus.map((menu) => {
             const isSelected = activeMenu === menu;
             return (
               <button
                 key={menu}
                 onClick={() => handleMenuClick(menu)}
-                className={`h-10 px-2.5 py-1.5 rounded-full flex justify-start items-center gap-[5px] transition-all ${
+                className={`h-10 px-2.5 py-1.5 rounded-full flex justify-start items-center gap-1 transition-all ${
                   isSelected
-                    ? 'pl-6 pr-2.5 bg-black-turtle shadow-green-turtle outline outline-1 outline-offset-[-1px] outline-green-turtle'
+                    ? 'pl-6 pr-2.5 bg-black-turtle outline outline-green-turtle'
                     : ''
                 }`}
+                style={isSelected ? {
+                  boxShadow: 'var(--shadow-green-turtle)'
+                } : {}}
               >
                 <div
                   className={`justify-start text-xs font-medium font-dm-sans leading-5 ${
@@ -67,11 +81,21 @@ export default function Controller({ selectedMenu = 'Leaderboard', onMenuChange 
                 </div>
                 {isSelected && (
                   <div className="w-5 h-5 relative">
-                    <div className="w-4 h-4 left-[1px] top-[1px] absolute">
+                    <div className="w-4 h-4 left-px top-px absolute">
                       {/* Outer glow circle */}
-                      <div className="w-3 h-3 left-[2.65px] top-[3px] absolute opacity-60 rounded-full shadow-[0px_0px_18.03px_0px_rgba(0,0,0,0.40)] shadow-[0px_0px_9.02px_0px_rgba(115,243,108,1.00)] shadow-[0px_3.61px_9.02px_0px_rgba(115,243,108,1.00)] border border-green-turtle"></div>
+                      <div 
+                        className="w-3 h-3 left-[3px] top-[3px] absolute opacity-60 rounded-full border border-green-turtle"
+                        style={{
+                          boxShadow: '0px 0px 18px 0px rgba(0,0,0,0.40), 0px 0px 9px 0px var(--green-turtle), 0px 4px 9px 0px var(--green-turtle)'
+                        }}
+                      ></div>
                       {/* Inner bright dot */}
-                      <div className="w-2 h-2 left-[4.80px] top-[5.15px] absolute bg-green-turtle rounded-full shadow-[0px_0px_18.03px_0px_rgba(115,243,108,1.00)] shadow-[0px_0px_36.07px_0px_rgba(115,243,108,1.00)]"></div>
+                      <div 
+                        className="w-2 h-2 left-[5px] top-[5px] absolute bg-green-turtle rounded-full"
+                        style={{
+                          boxShadow: '0px 0px 18px 0px var(--green-turtle), 0px 0px 36px 0px var(--green-turtle)'
+                        }}
+                      ></div>
                     </div>
                   </div>
                 )}
