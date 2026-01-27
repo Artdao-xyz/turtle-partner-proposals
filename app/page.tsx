01ae62edcae8dea098d6514899de07da14c1beb2
pedrofalco <fpfalco@gmail.com>
2026-01-27 10:06:49 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from './components/HeroSection';
 import ScrollSection from './components/ScrollSection';
-import TurtleFees from './components/TurtleFees';
+import LiquidityPrograms from './components/LiquidityPrograms';
 import VideoPlayer from './components/VideoPlayer';
 import AuthFormV2 from './components/AuthFormV2';
 
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="w-full h-svh lg:h-screen">
       <HeroSection />
       <ScrollSection />
-      <TurtleFees />
+      <LiquidityPrograms />
       <section className="w-full lg:min-h-screen flex justify-center items-center py-10 lg:py-0 lg:mt-26" style={{ backgroundColor: 'var(--black-turtle)' }}>
         <VideoPlayer
           loomId="https://www.loom.com/share/3a48bed3d1db4b888eaec015625d9f5e"
