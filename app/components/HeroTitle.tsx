e0b0f9c40465c89a6b5a74a7b2b8602ac394e16a
pedrofalco <fpfalco@gmail.com>
2026-02-09 15:31:55 -0300

    fix: Configure Montserrat font-family utility for Tailwind

--- app/components/HeroTitle.tsx
+++ app/components/HeroTitle.tsx
@@ -47,7 +47,7 @@ export default function HeroTitle({ isScrolled }: HeroTitleProps) {
         <div className="flex flex-col w-full items-center justify-center lg:min-w-[600px]">
           {/* Title */}
           <h1 className="relative z-10 font-montserrat text-center">
-            <span className="text-center font-montserrat text-5xl text-wise-white lg:text-7xl 2xl:text-8xl" style={{ lineHeight: '1.05', margin: 0, padding: 0 }}>
+            <span className="text-center font-montserrat font-medium text-5xl text-wise-white text-medium lg:text-7xl 2xl:text-8xl" style={{ lineHeight: '1.05', margin: 0, padding: 0 }}>
               Liquidity<br />Incentive<br />Toolkit
             </span>
           </h1>
